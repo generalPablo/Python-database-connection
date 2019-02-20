165ebdf5193cdb673b276127f9255f01677de0ef
generalPablo <onepablo05@gmail.com>
2019-02-20 20:26:55 +0200

    How to connect to oracle database

--- cx_oracle_db.py
+++ cx_oracle_db.py
@@ -12,7 +12,7 @@ class Oracle_Database:
         self.password = password
         self.sql_code = sql_code
 
-    def db_connect_oracle(self, run_date):
+    def db_connect_oracle(self, run_date=None):
         self.run_date = run_date
         # parameters to pass to the connection string for connecting to the database
         connection_variables = {'SERVER': self.hostname,
@@ -26,12 +26,45 @@ class Oracle_Database:
             connection = cx_Oracle.Connection(user=connection_variables['UID'], password=connection_variables['PWD'], dsn=dsn)
 
             # creating a date parameter to be passed into the query
-            param = {'rundate': self.run_date}
-            # creating a dataframe
+            if run_date != None:
+                param = {'rundate': self.run_date}
+                # creating a dataframe
 
-            data = pd.read_sql(self.sql_code,connection,params=param)
-            # close connection
-            connection.close()
-            return data
+                data = pd.read_sql(self.sql_code,connection,params=param)
+                # close connection
+                connection.close()
+                return data
+            else:
+                data = pd.read_sql(self.sql_code, connection)
+                # close connection
+                connection.close()
+                return data
         except:
-            print("Connection unsuccessful.\nCheck if you have entered the correct information for database connection string.")
+            print("Connection unsuccessful.\n"
+                  "Check if you have entered the correct information for database connection string.")
+
+
+#######################################################################################################################
+#                       Example of how to use the code                                                                #
+#######################################################################################################################
+
+# Example 1 - case where you pull data for a specific date
+# SQL code should look like the below
+
+sql_code = ('''SELECT INSERT_COLUMN_NAMES\
+                FROM INSERT_TABLE_NAME\
+                WHERE BUSINESSDATE =: rundate
+                ''')
+# notice that rundate is variable that we will bind to the query
+# Hence we defined a binding variable inside our class param = {'rundate': self.run_date}
+# You can extend the parameters, e.g if you might want to put two dates to get a range of data.
+# You will need to modify the code in the Class above
+
+
+conn = Oracle_Database("Host name or server","Database name", "port number", "username", "password",sql_code)
+
+date_example = '20190219'
+data = conn.db_connect_oracle(date_example)
+
+# Example 2 -  If no date is provided, then call the method as follows
+data2 = conn.db_connect_oracle()
